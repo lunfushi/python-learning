48b83d727f4626c035d08e9a1f8986f760806fbf
lunfushi <88506794@qq.com>
2026-05-30 00:36:38 +0800

    Add files via upload

---
+++ 课堂练习.py
@@ -0,0 +1,36 @@
+# def add_toppings(*toppings):
+#     print('你想添加的配料是：')
+#     for topping in toppings:
+#         print(topping)
+# add_toppings('奶酪')
+# print('-'*50)
+# add_toppings('培根','火腿')
+# print('-'*50)
+# add_toppings('草莓','鱼子酱','虾仁')
+
+# def build_profile(first,last,**user_info):
+#     '''创建一个字典，其中包含你的个人简介'''
+#     user_info['first_name'] = first
+#     user_info['last_name'] = last
+#     return user_info
+# you_profile = build_profile('杨','淑娟',city='上海',country='中国',msg='她正在进行python从入门到实战教学直播')
+# print(you_profile)
+# for v in you_profile.values():
+#     print(v)
+
+# def make_car(maker,types,**car_msg):
+#     car_msg['maker'] = maker
+#     car_msg['types'] = types
+#     return car_msg
+# car = make_car('宝马','轿车',color='蓝色',engin='八缸',seat='五座')
+# car1 = make_car('奔驰','SUV',color='红色')
+# car2 = make_car('奥迪','轿车',seat='五座',color='银色')
+# print(car)
+# print(car1)
+# print(car2)
+
+
+
+
+
+
